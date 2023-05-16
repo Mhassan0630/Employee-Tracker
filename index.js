01603f6641fe412520abe3f8c06d53476d53677a
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-16 05:55:50 -0500

    started on index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const database = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: Mh4k4f8f,d,
+        database: 'company_db'
+    },
+);
