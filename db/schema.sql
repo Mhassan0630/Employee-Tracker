2844dbca8df32b65d7e3c9257e7a17985020e360
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-16 05:47:03 -0500

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INTEGER REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
