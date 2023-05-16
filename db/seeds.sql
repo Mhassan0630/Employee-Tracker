f46990eba928773bcafd0e5e824e631697ed0800
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-16 05:49:47 -0500

    added roles for schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,13 @@ VALUES ("Athletics"),
        ("Legal"),
        ("Healthcare"),
        ("Finance");
+
+INSERT INTO roles (title, salary, department_id) VALUES
+('AthleticsManager', 200000, 1),
+('Athletic Director',80000, 1),
+('Legal Team Supervisor', 950000, 2),
+('Legal Director',650000,2 ),
+('Healthcare Chief Director', 850000, 3),
+('Medical Department Head', 500000, 3)
+('Chief Financial Officer', 400000, 4),
+('Finance Director',250000, 4)
