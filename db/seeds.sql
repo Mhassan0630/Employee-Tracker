62f7110ebb0e8c27ad14829314941ba5d473f8d0
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-25 21:34:18 -0500

    mooodified

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,7 +12,7 @@ INSERT INTO roles (title, salary, department_id) VALUES
 ('Healthcare Chief Director', 850000, 3),
 ('Medical Department Head', 500000, 3)
 ('Chief Financial Officer', 400000, 4),
-('Finance Director',250000, 4)
+('Finance Director',200000, 4)
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
--- index.js
+++ index.js
@@ -1,67 +1,103 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+import inquirer from 'inquirer';
+const database = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Asdfjk!2',
+    database: 'company_db'
+});
 
-const database = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: Mh4k4f8f,d,
-        database: 'company_db'
-    },
-);
+let roleList = [];
+let deptList = [];
+let empList = [];
+let mgrList = [];
 
-let roleDetails = [];
-let departmentDetails = [];
-let employeeDetails = [];
-let managerDetails = [];
+const initialQuestion = [{
+    type: 'list',
+    name: 'action',
+    message: "Select the action you want to perform?",
+    choices: [
+        'View All Employees', 'Add Employee', 'Update Employee Role', 'Update Employee Manager', 
+        'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 
+        'View Employees by Manager', 'View Employees by Department', 'Exit'
+    ]
+}];
 
-const initialOptions = [
-    {
-        type: 'list',
-        name: 'initial',
-        message: "What operation do you want to perform?",
-        choices: ['Display All Employees', 'Add New Employee', 'Edit Employee Role', 
-        'Edit Employee Manager', 'Display All Roles', 'Add New Role', 'Display All Departments', 'Add New Department', 
-        'Display Employees by Manager', 'Display Employees by Department', 'Exit']
-    }
-];
+function launchApp() {
+    console.log(`\n----------------------------------\n|         Employee        |\n|          Manager         |\n----------------------------------`);
+    loadRoleList();
+}
 
-initializeApplication();
-
-function initializeApplication() {
-    console.log("----------------------------------\n|         Employee Dashboard     |\n----------------------------------");
-    populateRoleDetails();   
-};
-
-function populateRoleDetails() {
-    database.query('SELECT id, title FROM role', function (error, results) {
+function loadRoleList() {
+    database.query('SELECT id, title FROM role', (error, results) => {
         if (error) throw error;
-        results.forEach(result => roleDetails.push({
-            title: result.title,
-            id: result.id
-        }));
-        populateDepartmentDetails()
-    })
-};
+        roleList = results.map(role => ({ title: role.title, id: role.id }));
+        loadDeptList();
+    });
+}
 
-function populateDepartmentDetails() {
-    database.query('SELECT name, id FROM department', function (error, results) {
+function loadDeptList() {
+    database.query('SELECT name, id FROM department', (error, results) => {
         if (error) throw error;
-        results.forEach(result => departmentDetails.push({
-            name: result.name,
-            id: result.id
-        }));
-        populateEmployeeDetails()
-    })
-};
+        deptList = results.map(department => ({ name: department.name, id: department.id }));
+        loadEmpList();
+    });
+}
 
-function populateEmployeeDetails() {
-    database.query("SELECT concat(employee.first_name,' ',employee.last_name) AS name, id FROM employee", function (error, results) {
+function loadEmpList() {
+    database.query("SELECT concat(employee.first_name,' ',employee.last_name) AS name, id FROM employee", (error, results) => {
         if (error) throw error;
-        results.forEach(result => employeeDetails.push({
-            name: result.name,
-            id: result.id
-        }));
-        populateManagerDetails()
-    })
-};
+        empList = results.map(employee => ({ name: employee.name, id: employee.id }));
+        loadMgrList();
+    });
+}
+
+function loadMgrList() {
+    database.query("SELECT concat(employee.first_name,' ',employee.last_name) AS name, id FROM employee", (error, results) => {
+        if (error) throw error;
+        mgrList = [{name: "None", id: 0}, ...results.map(manager => ({ name: manager.name, id: manager.id }))];
+        executeUserAction();
+    });
+}
+
+function executeUserAction() {  
+    inquirer.prompt(initialQuestion).then(response => {
+        switch(response.action) {
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'Add Employee':
+                addNewEmployee();
+                break;
+            case 'Update Employee Role':
+                changeEmployeeRole();
+                break;
+            case 'Update Employee Manager':
+                changeEmployeeManager();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add Role':
+                addNewRole();
+                break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'Add Department':
+                addNewDepartment();
+                break;
+            case 'View Employees by Manager':
+                viewEmployeesByManager();
+                break;
+            case 'View Employees by Department':
+                viewEmployeesByDepartment();
+                break;
+            default:
+                console.log('Goodbye');
+        }
+    });
+}
+
+// Now, you can implement the remaining functions like `viewEmployees`, `addNewEmployee`, `changeEmployeeRole`, `changeEmployeeManager`, `viewRoles`, `addNewRole`, `viewDepartments`, `addNewDepartment`, `view
